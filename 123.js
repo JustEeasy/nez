1ba09bcea4839757ea0f0ef18bf60c2b829bb111
JustEeasy <91015720+JustEeasy@users.noreply.github.com>
2021-12-13 19:30:13 +0200

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,34 @@
+//comment
+/*comment 1,
+comment 2*/
+console.log(
+"idiots bich men"
+)
+console.log(
+    'js'
+)
+var number=100
+let number2=25
+number3=10
+console.log(100)
+const year = 2021
+year2=`Happy new ${year} year!       `
+console.log(year2)
+name=prompt("ведите ваше некому ненужноя имя")
+console.log(name)
+alert(`здраствуй человек, ${name}! `)
+scores=0
+q1=prompt("ответе ви гей?: a) да в) нет")
+if(q1 == 'а'|| q1 == 'a'){
+    scores=scores + 4
+}
+q2 = prompt("ответе ви гей?: a) да в) нет")
+if(q2 == 'в'|| q2 == 'b'){
+    scores=scores + 9
+}
+else{
+scores=scores -7
+}
+
+alert(`${scores} `)
+
